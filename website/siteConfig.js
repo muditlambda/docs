3189cb6d5c7da8c223fbfb761fcc0f39bebf123f
psych0der <mayankbhola@gmail.com>
2018-09-27 17:16:28 +0530

    change project name for organization website

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "docs",
+  projectName: "LambdaTest.github.io",
   organizationName: "LambdaTest",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
